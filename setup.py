c384c36b419f8a82ee8bcb0607057c93cf855c53
Robin <robin.carl@fau.de>
2024-11-06 17:00:58 +0100

    fix: update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="math_quiz",                
+    version="1.0.0",                         
+    author="Robin Carl",                     
+    author_email="robcarl1997@gmail.com",
+    url="https://github.com/robcarl1997/dsss_homework_2",
+    python_requires='>=3.6',                 
+)
